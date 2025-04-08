4ccd719558d0f00b9fb0f22c84ce475d81b44b72
Ubuntu <ubuntu@ip-172-31-27-66.ap-southeast-1.compute.internal>
2025-04-08 01:42:54 +0000

    Modify backend to access mysql db on RDS

--- api/src/classes/DataSource.php
+++ api/src/classes/DataSource.php
@@ -1,7 +1,8 @@
 <?php
 class DataSource {
     
-    private static $hostname = "mysql";
+//    private static $hostname = "mysql";
+    private static $hostname = "database-1.ctg46iu4uqgt.ap-southeast-1.rds.amazonaws.com";
     private static $username = "foo";
     private static $password = "bar";
     private static $database = "svitts";
